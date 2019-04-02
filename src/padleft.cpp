d1ef38f6fbeb299ef2099a19c5c6dc10f4cc4195
Tom Breza <tom@pcservicegroup.co.uk>
2019-04-02 20:21:11 +0100

    Update padleft.cpp

--- src/padleft.cpp
+++ src/padleft.cpp
@@ -8,7 +8,7 @@ std::string pad_left(const std::string& str, int num_spaces)
 {
     std::string out = str;
     for (int i = 0; i < num_spaces; i++) {
-        out = " " + out;
+        out = " " + out; // this is rubbish ;)
     }
     return out;
 }
