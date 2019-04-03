4df6497f1cad11f4830dab2c1d313ab7b88b5000
Kasha <kmal362@yahoo.co.uk>
2019-04-03 23:39:37 +0100

    Tests passed.

--- src/padleft.cpp
+++ src/padleft.cpp
@@ -1,16 +1,19 @@
 
+
 #include <padleft.hpp>
 
 namespace pl {
 
+    std::string pad_left(const std::string &str, int num_spaces) {
 
-std::string pad_left(const std::string& str, int num_spaces)
-{
-    std::string out = str;
-    for (int i = 0; i < num_spaces; i++) {
-        out = " " + out; // this is rubbish ;)
+        std::string out = str;
+        std::string app = str;
+
+        for (int i = 0; i < num_spaces; ++i) {
+            app = " ";
+            out = app.append(out);
+        }
+        return out;
     }
-    return out;
-}
 
 }
