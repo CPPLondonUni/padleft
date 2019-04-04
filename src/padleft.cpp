92a50a69d20d39a2921be3544c05b1fe4e673721
Volodymyr Gladun <vgladun@gmail.com>
2019-04-04 10:52:32 +0100

    latest version

--- src/padleft.cpp
+++ src/padleft.cpp
@@ -1,18 +1,14 @@
-
 #include <padleft.hpp>
 
 namespace pl {
 
 
-std::string pad_left(const std::string& str, int num_spaces)
-{
-    std::string out = "";
-    for (int i = 0; i < num_spaces; i++) {
-        out += " ";
+    std::string pad_left(const std::string &str, int num_spaces) {
+        std::string out = str;
+        if (num_spaces > 0) {
+            out.insert(out.begin(), num_spaces, ' ');
+        }
+        return out;
     }
-    out += str;
-
-    return out;
-}
 
 }
