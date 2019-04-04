b68d02d96899a69a31b6781c9003c8df948f7f07
vglad <vgladun@gmail.com>
2019-04-04 11:02:10 +0100

    Update padleft.cpp

--- src/padleft.cpp
+++ src/padleft.cpp
@@ -1,9 +1,10 @@
+
 #include <padleft.hpp>
 
 namespace pl {
 
 
-std::string pad_left(const std::string &str, int num_spaces) 
+std::string pad_left(const std::string& str, int num_spaces)
 {
     std::string out = str;
     if (num_spaces > 0) {
