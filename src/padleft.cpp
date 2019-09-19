4e748dfe2210d30edf3d316cae8e2305198b92f1
contactonder <onderr@gmail.com>
2019-09-20 00:47:27 +0100

    padding line is updated by using insert method

--- src/padleft.cpp
+++ src/padleft.cpp
@@ -7,10 +7,11 @@ namespace pl {
 std::string pad_left(const std::string& str, int num_spaces)
 {
     std::string out = str;
-    for (int i = 0; i < num_spaces; i++) {
-        out = " " + out; // this is rubbish ;)
-    }
-    return out;
+   
+	if (num_spaces > 0)
+		out.insert(out.begin(), num_spaces, ' ');
+
+	return out;
 }
 
 }
