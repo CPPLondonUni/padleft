46b5b9450480328214990cfa4d2c409eafd85a42
Eugen0 <leahu_eugen@yahoo.com>
2020-05-28 19:42:50 +0100

    improved version

---
+++ cmake-build-debug/test/CMakeFiles/test_padleft.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Tania/CLionProjects/padleft/test/catch_main.cpp" "C:/Users/Tania/CLionProjects/padleft/cmake-build-debug/test/CMakeFiles/test_padleft.dir/catch_main.cpp.obj"
+  "C:/Users/Tania/CLionProjects/padleft/test/test_padleft.cpp" "C:/Users/Tania/CLionProjects/padleft/cmake-build-debug/test/CMakeFiles/test_padleft.dir/test_padleft.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Tania/CLionProjects/padleft/cmake-build-debug/CMakeFiles/padleft.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/test/CMakeFiles/test_padleft.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_padleft.dir/catch_main.cpp.obj"
+  "CMakeFiles/test_padleft.dir/test_padleft.cpp.obj"
+  "libtest_padleft.dll.a"
+  "test_padleft.exe"
+  "test_padleft.exe.manifest"
+  "test_padleft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_padleft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
